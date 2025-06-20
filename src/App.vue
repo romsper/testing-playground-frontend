19ab06a1349886067eb2d55d643fa44d64e5f90b
romsper <r.speranskii@gmail.com>
2025-06-20 01:15:24 -0400

    Added header and several new elements

--- src/App.vue
+++ src/App.vue
@@ -4,83 +4,111 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-      <md-filled-button type="submit">Sign in</md-filled-button>
+  <header class="header">
+    <img alt="Logo" class="logo" src="@/assets/logo.svg"/>
+    <div class="title">Brew & Bean</div>
+    <div class="actions">
+      <div class="links">
+        <RouterLink id="link" to="/">Products</RouterLink>
+        <RouterLink id="link" to="/about">Orders</RouterLink>
+        <RouterLink id="link" to="/contact">Contact</RouterLink>
+      </div>
+      <md-filled-button>
+        <RouterLink id="auth" to="/{{ link }}">Registration</RouterLink>
+      </md-filled-button>
+      <img alt="Avatar" class="avatar" src="@/assets/images/avatar.png"/>
     </div>
   </header>
 
-  <RouterView />
+  <div class="main-image">
+    <img src="@/assets/images/main-image.png" alt="Main Image" />
+    <div class="main-image-text">Welcome to Brew & Bean</div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.header {
+  width: 100vw;
+  align-items: center;
+  padding: 1rem;
+  background-color: white;
+  display: flex;
+  flex-direction: row;
+  border-bottom: gainsboro solid 0.5px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.header .actions {
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+  gap: 2rem;
 }
 
-nav {
+.header .links {
+  display: flex; 
+  gap: 2rem;
+  font-size: 1.2rem;
+}
+
+#link {
+  color: #333;
+  font-size: 1rem;
+  font-weight: bold;
+}
+
+#auth {
+  color: #333;
+  font-size: 1rem;
+  font-weight: bold;
+}
+
+.header .logo {
+  height: 40px;
+  width: 40px;
+}
+
+.header .avatar {
+  height: 60px;
+  width: 60px;
+}
+
+.title {
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #333;
+  margin-left: 1rem;
+}
+
+.main-image {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  height: 360px;
+  border-radius: 15px;
+  overflow: hidden;
+  margin: 1rem auto;
+  max-width: 1024px;
+  background: #fff;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.main-image img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.main-image-text {
+  position: absolute;
+  left: 16px;
+  bottom: 16px;
+  color: #fff;
+  font-weight: bold;
+  background: rgba(0,0,0,0.5);
+  padding: 8px 16px;
+  border-radius: 8px;
+  font-size: 1.5rem;
+  pointer-events: none;
 }
 </style>
